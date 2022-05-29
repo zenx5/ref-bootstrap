a876bb7394db86e5e78e094ef6497fb0a01d6533
Octavio Martinez <omartinez@logoscorp.com>
2022-05-28 21:16:01 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ref-bootstrap
+
+Package para hacer referencia a los archivos y clases de bootstrap en php
---
+++ class-import-file.php
@@ -0,0 +1,11 @@
+<?php
+
+
+class ImportFile
+{
+
+    public static function import($package = '',  $src = '', $typeFile = null)
+    {
+        echo __DIR__;
+    }
+}
